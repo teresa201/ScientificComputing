dd6522c858b8948444e7fc4d821991aa562f8eed
teresa201 <teresa201@me.com>
2017-09-26 19:03:21 -0500

    fix table4

--- SCP1/proj1_c.cpp
+++ SCP1/proj1_c.cpp
@@ -19,13 +19,12 @@ int main()
     double overF[26];
     double v0 = 1 - (1/ exp(1));
     bigV[0] = v0;
-   // printf("%1s %11s %15s \n", "j","V(j)", "V(j)/j!");
-   // printf("%-5d %-15f %-52f \n", 0,0,0);
 
     ofstream t1out("table1.txt", ios::out);
     if(!t1out){
        cout << "Unable to open file" << endl;
     }
+
     //print the first table of j, V(j), V(j)/j! for Vo = (1 - 1/e)
     for(int k = 1; k < 26; k++)
     {
@@ -39,9 +38,6 @@ int main()
     //vout.close();
     //overout.close();
     t1out.close();
-   // cout << " " << endl;
-   // cout << "Modified Formula error = .13" << endl;
-   // cout << " " << endl;
 
     //print modifed formula error = .13
     double mbigV[25];
@@ -49,10 +45,7 @@ int main()
     double error = .13;
     double mv0 = 1 - (1/ exp(1) + error);
     mbigV[0] = mv0;
-    //printf("%1s %11s %30s \n", "j","V(j)", "V(j)/j!");
-    //printf("%-5d %-30f %-52f \n", 0,0,0);
 
-    //print the table of j, V(j), V(j)/j! for Vo = (1 - 1/e + error)ofstream t1out("table1.txt", ios::out);
     ofstream t2out("table2.txt", ios::out);
     if(!t2out){
        cout << "Unable to open file" << endl;
@@ -64,12 +57,8 @@ int main()
         double moverfactorial = mbigV[k] / factorial(k);
         moverF[k] = moverfactorial;
         t2out << k << ' ' << mbigV[k] << ' ' << moverfactorial << endl;
-        //printf("%-5d %-30f %-52f \n", k, mbigV[k], moverfactorial);
     }
     t2out.close();
-   // cout << " " << endl;
-   // cout << "Modified Formula error = .0024" << endl;
-    //cout << " " << endl;
 
     //print modifed formula error = .0024
     double m1bigV[25];
@@ -77,13 +66,12 @@ int main()
     double error1 = .0024;
     double m1v0 = 1 - (1/ exp(1) + error1);
     m1bigV[0] = m1v0;
-    //printf("%1s %11s %30s \n", "j","V(j)", "V(j)/j!");
-    //printf("%-5d %-30f %-52f \n", 0,0,0);
 
     ofstream t3out("table3.txt", ios::out);
     if(!t3out){
        cout << "Unable to open file" << endl;
     }
+
     //print the table of j, V(j), V(j)/j! for Vo = (1 - 1/e + error)
     for(int k = 1; k < 25; k++)
     {
@@ -91,26 +79,21 @@ int main()
         double m1overfactorial = m1bigV[k] / factorial(k);
         m1overF[k] = m1overfactorial;
         t3out << k << ' ' << m1bigV[k] << ' ' << m1overfactorial << endl;
-        //printf("%-5d %-30f %-52f \n", k, m1bigV[k], m1overfactorial);
     }
     t3out.close();
-   // cout << " " << endl;
-   // cout << "Modified Formula error = .000035" << endl;
-   // cout << " " << endl;
 
     //print modifed formula error = .000035
     double m2bigV[25];
     double m2overF[25];
-    double error2 = .0024;
+    double error2 = .000035;
     double m2v0 = 1 - (1/ exp(1) + error2);
     m2bigV[0] = m2v0;
-    //printf("%1s %11s %30s \n", "j","V(j)", "V(j)/j!");
-    //printf("%-5d %-30f %-52f \n", 0,0,0);
 
     ofstream t4out("table4.txt", ios::out);
     if(!t4out){
        cout << "Unable to open file" << endl;
     }
+
     //print the table of j, V(j), V(j)/j! for Vo = (1 - 1/e + error)
     for(int k = 1; k < 25; k++)
     {
@@ -118,6 +101,5 @@ int main()
         double m2overfactorial = m2bigV[k] / factorial(k);
         m2overF[k] = m2overfactorial;
         t4out << k << ' ' << m2bigV[k] << ' ' << m2overfactorial << endl;
-        //printf("%-5d %-30f %-52f \n", k, m2bigV[k], m2overfactorial);
     }
 }
